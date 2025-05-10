8b5bccc22ca513a07e0cf29b3e9af845eace6d2c
pinge2 <katash.kirill10@gmail.com>
2025-05-11 00:32:39 +0300

    Update joss.cpp

--- strategies/joss.cpp
+++ strategies/joss.cpp
@@ -5,6 +5,6 @@ namespace strat{
   bool joss(bool pos, const GameHistory& game){
     if (game.empty()) return 1;
     bool think = pos == 0 ? game.back().second : game.back().first;
-    return 
+    return rand() % 11 == 1 ? 0 : think;
   }
 }
