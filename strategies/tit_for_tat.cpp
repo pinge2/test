537b9d6624683824d105dcb5dfb2d66307e43bb1
pinge2 <katash.kirill10@gmail.com>
2025-05-11 00:20:31 +0300

    Update tit_for_tat.cpp

--- strategies/tit_for_tat.cpp
+++ strategies/tit_for_tat.cpp
@@ -1,3 +1,10 @@
 #include "strategy.hpp"
 
 
+
+namespace strat{
+  bool tit_for_tat(bool pos, const GameHistory& game){
+    if (game.empty()) return 1;
+    return pos == 0 ? game.back().second : game.back().first;
+  }
+}
