761b0913bc0092ea020b918c3a0c4e72c854d57d
pinge2 <katash.kirill10@gmail.com>
2025-05-11 10:11:53 +0300

    Create strategies.hpp

---
+++ strategies.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "strategies/tit_for_tat.cpp"
+#include "strategies/joss.cpp"
+#include "strategies/tester.cpp"
+#include "strategies/grasscamp.cpp"
+#include "strategies/wolf.cpp"
+#include "strategies/sample.cpp"
+#include "strategies/friedman.cpp"
+#include "strategies/juggernaut.cpp"
