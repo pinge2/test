40eebf994361feec69f10c47c46c358b0201841a
pinge2 <katash.kirill10@gmail.com>
2025-05-11 10:09:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "game.cpp"
+#include "strategies.hpp"
 
 
 GameStrategy strategies[] = {
-  strat::tit_for_tat, strat::
+  strat::tit_for_tat, strat::friedman, strat::joss,
+  strat::grasscamp, strat::tester, strat::sample,
+  strat::random, strat::wolf, strat::juggernaut
 };
 
 
