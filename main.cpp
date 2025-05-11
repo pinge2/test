abb5302aa523f283d118b6511c4a9c87554063d6
pinge2 <katash.kirill10@gmail.com>
2025-05-11 10:15:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,17 @@ GameStrategy strategies[] = {
   strat::random, strat::wolf, strat::juggernaut
 };
 
+int results[9] = {};
+
 
 int main(){
   Game main_tour;
-  
+
+  for (int i = 0; i < 9; ++i){
+    for (int j = 0; j < 9; ++j){
+      main_tour.set_strategies(strategies[i], strategies[j]);
+      main_tour.play(200);
+      results[i] += main_tour.get_results().first;
+    }
+  }
 }
